c3dd69677056a2085a117370162e28cce940e0f4
Orinami Olatunji <orinamiolatunji@gmail.com>
2024-04-23 10:49:05 +0100

    fix lint issues

--- packages/twenty-front/src/modules/ui/input/components/internal/date/components/TimeInput.tsx
+++ packages/twenty-front/src/modules/ui/input/components/internal/date/components/TimeInput.tsx
@@ -8,8 +8,8 @@ import { TIME_BLOCKS } from '@/ui/input/components/internal/date/constants/TimeB
 import { TIME_MASK } from '@/ui/input/components/internal/date/constants/TimeMask';
 
 const StyledIconClock = styled(IconClockHour8)`
+  color: ${({ theme }) => theme.font.color.secondary};
   position: absolute;
-  color: ${({ theme }) => theme.font.color.secondary}};
 `;
 
 const StyledTimeInputContainer = styled.div`
